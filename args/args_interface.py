51485b0765519d30a92f3411757f3e7d3d5d4e8f
kilianfriedrich <friedrich.kilian@outlook.de>
2019-04-28 19:19:39 +0200

    Bugfixes + new bugs to fix later (Actually no, it should be bugfree now)

--- args/args_interface.py
+++ args/args_interface.py
@@ -14,6 +14,10 @@ def get_str(str_title):
 def get_list(list_title):
 
     value = arg_values.get(list_title, None)
+    if not isinstance(value, list):
+
+        value = [value]
+
     return list(value) if value is not None else list()
 
 
