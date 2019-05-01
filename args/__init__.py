2ce0b1a9d108dfe421c120aa3a15c52e8bb86d40
kilianfriedrich <friedrich.kilian@outlook.de>
2019-05-01 15:39:13 +0200

    Renamed arg_values to ARG_V and arg_booleans to ARG_B (because it looks cooler)

--- args/__init__.py
+++ args/__init__.py
@@ -16,8 +16,8 @@
 
 from sys import argv  # arguments which are passed on the command line are stored in this 'argv' array
 
-arg_values = dict()  # stores values
-arg_booleans = list()  # stores booleans
+ARG_V = dict()  # stores values
+ARG_B = list()  # stores booleans
 
 __cur_key__ = None  # stores the name of the current value so that contents can be added to it
 for arg in argv:
@@ -25,15 +25,15 @@ for arg in argv:
     if arg.startswith('--'):
 
         __cur_key__ = None  # clear current value
-        arg_booleans.append(arg.lstrip('--'))  # add argument to (global) list of booleans
+        ARG_B.append(arg.lstrip('--'))  # add argument to (global) list of booleans
 
     elif arg.startswith('-'):
 
         __cur_key__ = arg.lstrip('-')
-        arg_values[__cur_key__] = list()  # adds key to dict & assigns an empty list (/clears entry if already there)
+        ARG_V[__cur_key__] = list()  # adds key to dict & assigns an empty list (/clears entry if already there)
 
     # argument is neither a boolean nor a key
     # just continue if there is a current key to assign the value to
     elif __cur_key__ is not None:
 
-        arg_values[__cur_key__].append(arg)  # appends value to key
+        ARG_V[__cur_key__].append(arg)  # appends value to key
