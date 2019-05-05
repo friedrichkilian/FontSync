68f790fbcb865a9a5269e93262c17affbef53d1a
kilianfriedrich <friedrich.kilian@outlook.de>
2019-05-05 18:07:18 +0200

    Comments and a bit of optimization

--- settings/__init__.py
+++ settings/__init__.py
@@ -13,7 +13,7 @@ from pickle import dump, load
 # the path to this folder (needed bc the setting file is located here)
 SCRIPT_DIR = dirname(__file__) + '/'
 
-SETTINGS_FILE_NAME = 'user_settings'  # change this if filename changes
+SETTINGS_FILE_NAME = 'settings'  # change this if filename changes
 SETTINGS_FILE_PATH = SCRIPT_DIR + SETTINGS_FILE_NAME
 SETTINGS = load(open(SETTINGS_FILE_PATH, 'rb'))  # open dict in file via pickle.load()
 
