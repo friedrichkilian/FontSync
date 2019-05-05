111a756b9a83172cefa6cb53218a83c01984d876
Kilian Friedrich <38128547+friedrichkilian@users.noreply.github.com>
2019-05-05 18:07:53 +0200

    Delete backend.py

--- settings/backend.py
+++
@@ -1,9 +0,0 @@
-from settings import *
-#
-print(USER_SETTINGS)
-#
-new_settings = {'output_dir': '/Library/Fonts/GoogleFonts/{}', 'output_dir_space_replacement': ''}
-#
-# if new_settings.__len__() > 0:
-#
-#     dump(new_settings, open(USER_SETTINGS_FILE_PATH, 'wb'))
